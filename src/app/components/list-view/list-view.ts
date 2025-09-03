c6c8af9875fc018766ded860c661eb4796fa2d4d
Anill007 <anil.123aaaaaa@gmail.com>
2025-09-03 09:53:46 +0545

    feat(component): list-view of the users
    - navigate to user
    - list view and responsive design

---
+++ src/app/components/list-view/list-view.ts
@@ -0,0 +1,19 @@
+import { Component, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '../../models/user-model';
+
+@Component({
+  selector: 'app-list-view',
+  imports: [],
+  templateUrl: './list-view.html',
+  styleUrl: './list-view.scss'
+})
+export class ListView {
+  public users = input.required<IUser[]>();
+
+  constructor(private router: Router) {}
+
+  public navigateToDetail(userId: number) {
+    this.router.navigate(['/user', userId]);
+  }
+}
